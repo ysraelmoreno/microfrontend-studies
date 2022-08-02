a1e88d7432b9b1ea36f69bbcad6e335490e61fad
Ysrael Moreno <ysraelmoreno02@gmail.com>
2022-08-01 22:42:31 -0300

    fix publicPath on webpack config

--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 module.exports = {
   output: {
-    publicPath: "http://localhost:3002/",
+    publicPath: "https://cart-mf.netlify.app/",
   },
 
   resolve: {
